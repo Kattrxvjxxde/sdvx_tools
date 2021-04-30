cd11686290867eed5b7fa7cb273ccf26142ac16a
wanirou <wanirou14titech@gmail.com>
2021-04-30 19:45:39 +0900

    rubocop警告解消

--- app/models/mst_music.rb
+++ app/models/mst_music.rb
@@ -1,5 +1,5 @@
 class MstMusic < ApplicationRecord
   has_many :mid_music_music_genres, dependent: :destroy
   has_many :mst_music_genres, through: :mid_music_music_genres
-  has_many :mst_charts
+  has_many :mst_charts, dependent: :destroy
 end
